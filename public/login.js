936b4f44c82c638251217294b8c3995dd8ee34a4
davidsumsion <davidsumsion20@gmail.com>
2024-03-23 13:40:42 -0600

    fix: anonomous function in login.js

--- public/login.js
+++ public/login.js
@@ -5,17 +5,17 @@
 //   }
   
 
-  (async () => {
-    const userName = localStorage.getItem('userName');
-    if (userName) {
-      document.querySelector('#playerName').textContent = userName;
-      setDisplay('loginControls', 'none');
-      setDisplay('playControls', 'block');
-    } else {
-      setDisplay('loginControls', 'block');
-      setDisplay('playControls', 'none');
-    }
-  })();
+  // (async () => {
+  //   const userName = localStorage.getItem('userName');
+  //   if (userName) {
+  //     document.querySelector('#playerName').textContent = userName;
+  //     setDisplay('loginControls', 'none');
+  //     setDisplay('playControls', 'block');
+  //   } else {
+  //     setDisplay('loginControls', 'block');
+  //     setDisplay('playControls', 'none');
+  //   }
+  // })();
   
   async function loginUser() {
     loginOrCreate(`/api/auth/login`);
